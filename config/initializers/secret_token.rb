117e25dfb5f9403f181de14b8bca8975bbaf22c7
niwat0ri <niwatori@gmail.com>
2012-03-01 11:04:22 +0700

    First commit nih

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kookoopoo::Application.config.secret_token = '5b9f8b08599755afcb79f568b6a7062802820c11680639b29003ddf57bcf3faa4489ebb5c336a24fd951b7828e51b2489cf6bc77777a1a505f2c70dc42b99c9a'
