c207af25c8baa50a4dd146f5aa07105b848bb07e
Reta <retanugraha@rocketmail.com>
2012-05-04 14:35:13 +0700

    Atur listing rute persalesman jadi by-tanggal rev2

--- app/controllers/routelist_controller.rb
+++ app/controllers/routelist_controller.rb
@@ -3,7 +3,8 @@ class RoutelistController < ApplicationController
   # GET /salesmen.json
   def index
     @paramsalesmanid = params[:salesman_id]
-    @visiting_route = VisitingRoute.find(:all, :conditions=>{:salesman_id=>@paramsalesmanid, :created_at=>Time.now.strftime("%Y-%m-%d")})
+    # @visiting_route = VisitingRoute.find(:all, :conditions=>{:salesman_id=>@paramsalesmanid, :created_at=>Time.now.strftime("%Y-%m-%d")})
+	@visiting_route = VisitingRoute.find(:all, :conditions=>["salesman_id=? AND DATE(created_at)=?", @paramsalesmanid, Time.now.strftime("%Y-%m-%d")])
     respond_to do |format|
       format.html { render :index }
       format.json { render :partial => "routelist/index.json" }
@@ -13,7 +14,8 @@ class RoutelistController < ApplicationController
   def search
     @paramsalesmanid = params[:salesman_id]
 	@paramscreated_at = params[:created_at]
-    @visiting_route = VisitingRoute.find(:all, :conditions=>{:salesman_id=>@paramsalesmanid, :created_at=>@paramscreated_at})  
+    # @visiting_route = VisitingRoute.find(:all, :conditions=>{:salesman_id=>@paramsalesmanid, :created_at=>@paramscreated_at})
+	@visiting_route = VisitingRoute.find(:all, :conditions=>["salesman_id=? AND DATE(created_at)=?", @paramsalesmanid, @paramscreated_at])  
     respond_to do |format|
       format.html { render :index }
       format.json { render :partial => "routelist/index.json" }
